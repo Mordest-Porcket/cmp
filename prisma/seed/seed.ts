5535ebd52645aaca95401eea3d326e76d2afe95a
Joseph Ogayo <mordestporcket27@gmail.com>
2025-09-08 19:40:00 +0300

    activating prisma files

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -2,7 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  console.log("Test!");
+}
 
 main()
   .catch((e) => {
